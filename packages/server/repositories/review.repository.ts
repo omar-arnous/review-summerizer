e917edfce73cbe5da6ebe671a52e2e087564e349
omar arnous <omar99.arnous@gmail.com>
2025-09-08 13:11:40 +0300

    handle regeneration

--- packages/server/repositories/review.repository.ts
+++ packages/server/repositories/review.repository.ts
@@ -29,4 +29,10 @@ export const reviewRepository = {
       update: data,
     });
   },
+
+  getReviewSummary(productId: number) {
+    return prisma.summary.findUnique({
+      where: { productId },
+    });
+  },
 };
